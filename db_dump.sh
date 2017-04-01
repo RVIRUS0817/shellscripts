84992578b20932e98b942a1ed8b08eabab883974
RVIRUS0817 <t.virus.1989@gmail.com>
2017-04-01 23:22:28 +0900

    fix scripts

--- db_dump.sh
+++ db_dump.sh
@@ -7,3 +7,30 @@ LOG=mysql-`date +\%Y-\%m-\%d --date "-1 days"`.log
 mysqldump -h clientIP -u root -x -ppassword --all-databases > $DUMP
 mysql -h MyDBIP -u root -ppassword < $DUMP
 rm -rf $OLD_FILE $LOG
+
+---------------or
+
+# /bin/sh
+
+OUTPUTDIR="/home/adachin/db_back"
+for x in hogedb; do
+
+  FILE=$OUTPUTDIR/$x.`date +\%Y-\%m-\%d`.sql.gz
+  OLD_FILE=$OUTPUTDIR/$x.`date +\%Y-\%m-\%d --date "-30 days"`.sql.gz
+  mysqldump -h localhost -u root $x | gzip > ${FILE}
+
+  rm -f ${OLD_FILE}
+done
+
+-----or
+
+# /bin/sh
+
+DUMP=/home/adachin/db_back/db-`date +\%Y-\%m-\%d`.sql
+
+mysqldump -h localhost -u root $x | gzip > ${FILE}
+
+-
+crontab -l
+00 04 * * * find /db-* -mtime +7 -exec rm -f {} \;
+
