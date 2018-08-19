dafd3559d4be8d707d96332f76c5ea5b2d8d3622
RVIRUS0817 <t.virus.1989@gmail.com>
2018-08-19 17:39:50 +0900

    fix mysqldump

--- backup_script/mysql/mysqldump.sh
+++ backup_script/mysql/mysqldump.sh
@@ -8,7 +8,7 @@ OLD_DATE=`date +%Y%m%d --date "5 days ago"`
 DUMP=$DIR/mysqldump.$DATE.sql
 OLD_DUMP=$DIR/mysqldump.$OLD_DATE.sql
 
-mysqldump -u root -x -pxxxx --all-databases > $DUMP
+mysqldump -u root -pxxxx --all-databases > $DUMP
 
 rm -f $OLD_DUMP
 
