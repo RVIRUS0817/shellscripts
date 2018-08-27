192c4a4542b225b48dc358e79a28b6e3bad5c90f
RVIRUS0817 <t.virus.1989@gmail.com>
2018-08-27 10:18:45 +0900

    :sparkles: add vuls-update5.sh

---
+++ vuls_script/vuls-update5.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+set -e
+
+GOCVEDICTIONARY=$GOPATH/src/github.com/kotakanbe/go-cve-dictionary
+GOVALDICTIONARY=$GOPATH/src/github.com/kotakanbe/goval-dictionary
+GOST=$GOPATH/src/github.com/knqyf263/gost
+VULS=$GOPATH/src/github.com/future-architect/vuls
+
+## Current goval/go-cve-dictionary,Vuls version
+
+echo -e "----Current goval/go-cve-dictionary/gost,Vuls version----"
+go-cve-dictionary -v
+goval-dictionary -v
+gost -v
+vuls -v
+
+## Update go-cve-dictionary
+
+echo -e "\n----Update go-cve-dictionary----"
+cd $GOCVEDICTIONARY && git pull > /dev/null 2>&1 && rm -rf vendor && rm -rf $GOPATH/pkg && make install > /dev/null 2>&1
+
+if [ $? = 0 ]; then
+  echo "Update OK"
+else
+  echo "Update NO!!"
+fi
+
+## Update goval-dictionary
+
+echo -e "\n----Update goval-dictionary----"
+cd $GOVALDICTIONARY && git pull  > /dev/null 2>&1  && rm -rf vendor && rm -rf $GOPATH/pkg && make install > /dev/null 2>&1
+
+if [ $? = 0 ]; then
+  echo "Update OK"
+else
+  echo "Update NO!!"
+fi
+
+## Update gost
+
+echo -e "\n----Update gost----"
+cd $GOST && git pull  > /dev/null 2>&1  && rm -rf vendor && rm -rf $GOPATH/pkg && make install > /dev/null 2>&1
+
+if [ $? = 0 ]; then
+  echo "Update OK"
+else
+  echo "Update NO!!"
+fi
+
+
+## Update Vuls
+
+echo -e "\n----Update Vuls----"
+cd $VULS && git pull  > /dev/null 2>&1 && rm -rf vendor && rm -rf $GOPATH/pkg && make install > /dev/null 2>&1
+
+if [ $? = 0 ]; then
+  echo "Update OK"
+else
+  echo "Update NO!!"
+fi
+
+## New goval/go-cve-dictionary/gost,Vuls version
+
+echo -e "\n----New goval/go-cve-dictionary,Vuls version----"
+go-cve-dictionary -v
+goval-dictionary -v
+gost -v
+vuls -v
