103ab9d39b2a7fe99f751538527b65499568486e
RVIRUS0817 <t.virus.1989@gmail.com>
2019-03-07 14:48:35 +0900

    fix bigquery_script

--- bigquery_script/backup-all-views.sh
+++ bigquery_script/backup-all-views.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
-DATE=`date +%Y%m%d`
-OLD_DATE=`date +%Y%m%d --date "5 days ago"`
-
+ 
+DIR=/home/adachin/backup/views/
+ 
+DATE_YEAR=`date +%Y`
+DATE=`date +%m%d`
+OLD_DATE=`date +%m%d --date "5 days ago"`
+ 
 DATASETS=$(bq  ls --format=sparse | tail -n+3)
+ 
+mkdir -p $DIR/$DATE_YEAR/$DATE
 for d in $DATASETS; do
   TABLES=$(bq ls --format=prettyjson "$d" | jq '.[] | "\(.id), \(.type)"')
   IFS=$'\n'
@@ -11,7 +16,7 @@ for d in $DATASETS; do
     [[ ! "$table" == *VIEW* ]] && continue
     view=$(echo "$table" | sed -e 's/"//g' | cut -d , -f 1)
     query=$(bq show --format=prettyjson "$view" | jq -r '.view.query')
-    echo -e "$query" > "/home/adachin/backup/views/$view.sql.$DATE"
-    rm -rf "/home/adachin/backup/views/$view.sql.$OLD_DATE"
+    echo -e "$query" > "$DIR/$DATE_YEAR/$DATE/$view.sql"
+    rm -rf "$DIR/$DATE_YEAR/$OLD_DATE"
   done
 done
