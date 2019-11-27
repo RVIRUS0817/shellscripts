32ec4cc4fac52e9dd78bfd163a6c340835e95c27
RVIRUS0817 <t.virus.1989@gmail.com>
2019-11-27 13:49:19 +0900

    fix bigquery-to-mysql.sh

---
+++ digdag_script/.common/secrets.sh
@@ -0,0 +1,6 @@
+echo readonly USER='xxxxxxx'
+echo readonly PWD='xxxxxxxxxxxxx'
+echo readonly HOST='xxxxxxxxxxxxxxxx'
+echo readonly PORT=xxxxxxxx
+
+
--- digdag_script/bigquery-to-mysql.sh
+++ digdag_script/bigquery-to-mysql.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+$(".common/secrets.sh")
 YESTERDAY=$(date +%Y-%m-%d -d '1 days ago')
-DBPass=xxxxxxxxxx
 
-## out csv
-bq query --use_legacy_sql=false --format=csv --max_rows=100000 "SELECT * FROM \`prd-adachin.views.views\` where dt=\"${YESTERDAY}\"" > views.csv
+## views out csv
+bq query --use_legacy_sql=false --format=csv --max_rows=100000 "SELECT * FROM \`prd-adachin.views.views\` where dt=\"${YESTERDAY}\"" > /tmp/views.csv
 
 ## add ,
-sed -i '1,2d' views.csv
-sed -i "s/^/,/g" views.csv
+sed -i '1,2d' /tmp/views.csv
+sed -i "s/^/,/g" /tmp/views.csv
 
 # restore
-mysql -h xxxxxxxxxx -u xxxxxxx -p${DBPass} -P xxxxxx adachin -N -e "LOAD DATA LOCAL INFILE '/home/views.csv' INTO TABLE daily_views FIELDS TERMINATED BY ','"
+mysql -h "${HOST}" -u lancers -p"${PWD}" -P "${PORT}" adachin -N -e "LOAD DATA LOCAL INFILE '/tmp/views.csv' INTO TABLE views FIELDS TERMINATED BY ','"
 
-## delete views.csv
-rm -rf views.csv
+## rm profile_views.
+rm -f /tmp/views.csv
