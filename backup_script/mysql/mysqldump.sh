a9ccd29dfeb5945b6478cc051de2e7dda1c26532
RVIRUS0817 <t.virus.1989@gmail.com>
2020-01-24 08:45:55 +0900

    fix db shell

--- backup_script/mysql/mysqldump.sh
+++ backup_script/mysql/mysqldump.sh
@@ -5,10 +5,9 @@ DIR=/root/mount-s3/backup
 DATE=`date +%Y%m%d`
 OLD_DATE=`date +%Y%m%d --date "5 days ago"`
 
-DUMP=$DIR/mysqldump.$DATE.sql
-OLD_DUMP=$DIR/mysqldump.$OLD_DATE.sql
+DUMP=${DIR}/mysqldump.${DATE}.sql
+OLD_DUMP=${DIR}/mysqldump.${OLD_DATE}.sql
 
-mysqldump -u root -pxxxx --all-databases > $DUMP
+mysqldump -u root -pxxxx --all-databases > ${DUMP}
 
-rm -f $OLD_DUMP
-
+rm -f ${OLD_DUMP}
