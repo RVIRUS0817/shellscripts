d839d4a681c6a5e8f2a8e25557ab7f9158f0096d
RVIRUS0817 <t.virus.1989@gmail.com>
2020-08-29 01:24:17 +0900

    add vagrant

---
+++ vagrant/deploy-vagrant.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+STATUSNAME=$1
+DIR=~/vagrant/stg
+
+help() {
+echo "
+  下記のようにstatusを指定して実行しろや！！
+  (例) ./vagrant.sh status,up,suspend
+    "
+}
+
+if [ $# -ne 1 ];then
+ help
+exit
+fi
+
+cd ${DIR} && vagrant ${STATUSNAME}
+
