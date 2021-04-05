acab062477d0b8251bf4479607df0561b8503fe2
RVIRUS0817 <t.virus.1989@gmail.com>
2021-04-05 18:02:00 +0900

    fix

--- aws/ecs-exec/ecs-exec.sh
+++ aws/ecs-exec/ecs-exec.sh
@@ -18,12 +18,12 @@ fi
  
 COUNT_TASK=$(aws --profile=${PROFILE} ecs list-tasks --cluster ${PROFILE} --service-name ${PROFILE}-app --output text | sed -e 's/\s/ /g' | cut -d ' ' -f2 | cut -c 46-)
 echo "現在のAppコンテナ数"
-echo ${COUNT_TASK}
+echo "${COUNT_TASK}"
  
-TASK=$(aws --profile=${PROFILE} ecs list-tasks --cluster ${PROFILE} --service-name ${PROFILE}-app --output text | sed -e 's/\s/ /g' | cut -d ' ' -f2 | cut -c 46- |sed -e '2d')
+TASK=$(aws --profile=${PROFILE} ecs list-tasks --cluster ${PROFILE} --service-name ${PROFILE}-app --output text | sed -e 's/\s/ /g' | cut -d ' ' -f2 | cut -c 46- |sed -e '2,$d')
 echo -e ""
 echo "・login"
-echo ${TASK}
+echo "${TASK}"
  
 aws --profile=${PROFILE} ecs execute-command  \
     --region ap-northeast-1 \
