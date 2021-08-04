eedd3205827d53b53ea5ad2553808d67a4c666b7
RVIRUS0817 <t.virus.1989@gmail.com>
2021-08-04 15:08:48 +0900

    fix

--- backup_script/mysql/mysqldump.sh
+++ backup_script/mysql/mysqldump.sh
@@ -1,13 +1,20 @@
 #/bin/bash
 
-DIR=/root/mount-s3/backup
+# vim ~/.common/secrets.sh
+#echo readonly USER='hoge'
+#echo readonly PWD='hoge'
+#echo readonly HOST='hoge'
 
+set -eu
+
+$("~/.common/secrets.sh")
+
+DIR=/root/backup
 DATE=`date +%Y%m%d`
 OLD_DATE=`date +%Y%m%d --date "5 days ago"`
-
 DUMP=${DIR}/mysqldump.${DATE}.sql
 OLD_DUMP=${DIR}/mysqldump.${OLD_DATE}.sql
 
-mysqldump -u root -pxxxx --all-databases > ${DUMP}
+mysqldump -v --skip-column-statistics --skip-lock-tables -h "${HOST}" -u "${USER}" -p"${PWD}" hoge_db | gzip > ${DUMP}
 
 rm -f ${OLD_DUMP}
